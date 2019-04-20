0f0de41868833365c0876ac6e3588956cd6d8b3c
kybas <sergeykiba2012@gmail.com>
2019-04-21 01:47:15 +0300

    switch to chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -62,7 +62,7 @@ if @browser == 'firefox'
 elsif @browser == 'chrome'
   Capybara.register_driver :selenium do |app|
     options = Selenium::WebDriver::Chrome::Options.new
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-popup-blocking')
